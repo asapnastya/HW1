cd663ce28697b34be5cd9c477bec4d68e6f8d980
Анастасия Романова <>
2024-09-29 12:31:02 +0300

    feat: launch && main screens

---
+++ HW1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW1
+//
+//  Created by Анастасия Романова on 9/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
